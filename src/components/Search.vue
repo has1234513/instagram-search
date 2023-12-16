64e1671e696c03ec2f15a88c44b1078557d9ef6a
has1234513 <hashaydar29@gmail.com>
2023-12-16 12:43:05 +0200

    updarer

---
+++ src/components/Search.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="bg-white p-4 md:p-6 rounded-lg shadow-lg">
+    <h2 class="text-xl md:text-2xl font-semibold mb-4">Find and Aggregate Instagram UGC (User Generated Content)</h2>
+    <div class="relative rounded-full border border-gray-300 p-3">
+      <input
+        v-model="searchTerm"
+        class="w-full outline-none placeholder-gray-500"
+        type="text"
+        placeholder="Enter hashtags(#trend...) or mentions(@user...)"
+      />
+      <button
+        @click="search"
+        class="absolute right-2 top-1/2 transform -translate-y-1/2 bg-blue-500 text-white px-4 py-2 md:px-6 md:py-3 rounded-full hover:bg-blue-600 text-sm md:text-base">
+        Search
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchTerm: "",
+    };
+  },
+  methods: {
+    search() {
+      // Implement search functionality here
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Tailwind handles most styling, so additional styles might not be needed */
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Search from '../components/Search.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="font-sans text-black h-screen bg-white pt-20 flex flex-col items-center">
+    <Search/>
+    <!-- Content Display Area -->
+    <div class="w-full max-w-4xl mt-8 px-4">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <!-- Dynamic content goes here -->
+        <!-- Example: -->
+        <div class="bg-gray-200 p-4 rounded-lg shadow">
+          <p>Content 1</p>
+        </div>
+        <div class="bg-gray-200 p-4 rounded-lg shadow">
+          <p>Content 2</p>
+        </div>
+        <!-- ... more content blocks -->
+      </div>
+    </div>
+  </div>
 </template>
